36a8917104e7879e0c15f877a947d95745a53760
Tarek-Soliman-7 <solimant011@gmail.com>
2025-11-14 20:47:15 +0200

    Add Angular front-end and other new files

---
+++ Core/Services/Implementations/ServiceManagerWithFactoryDelegate.cs
@@ -0,0 +1,22 @@
+﻿using Services.Abstraction.Contracts;
+
+namespace Services.Implementations
+{
+    public class ServiceManagerWithFactoryDelegate(Func<IProductService> _productFactory,
+        Func<IOrderService> _orderFactory, Func<IBasketService> _basketFactory,
+        Func<IPaymentService> _paymentFactory, Func<IAuthenticationService> _authFactory
+        , Func<ICashService> _cashFactory) : IServiceManger
+    {
+        public IProductService ProductService => _productFactory.Invoke();
+
+        public IBasketService BasketService => _basketFactory.Invoke();
+
+        public IAuthenticationService AuthenticationService => _authFactory.Invoke();
+
+        public IOrderService OrderService => _orderFactory.Invoke();
+
+        public IPaymentService PaymentService => _paymentFactory.Invoke();
+
+        public ICashService CashService => _cashFactory.Invoke();
+    }
+}
---
+++ Infrastructure/Presentation/Attributes/RedisCasheAttribute.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Services.Abstraction.Contracts;
+using System.Text;
+
+namespace Presentation.Attributes
+{
+    internal class RedisCasheAttribute(int durationInSeconds = 120 ) : ActionFilterAttribute
+    {
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var cashService = context.HttpContext.RequestServices.GetRequiredService<IServiceManger>().CashService;
+            //Data cashed or not ==> Key
+            //Key ==> PathUrl + query string
+            //context.HttpContext.Request.Path // /api/products
+            //context.HttpContext.Request.Query // key , value ==> Sort --> NameDesc , PageSize --> 10
+            string key = GenerateKey(context.HttpContext.Request);
+            var result = await cashService.GetCashedValueAsync(key);
+            if (result != null)
+            {
+                context.Result = new ContentResult
+                {
+                    Content = result,
+                    ContentType = "application/json",
+                    StatusCode = StatusCodes.Status200OK
+                };
+                return;
+            }
+            var resultContext = await next.Invoke();
+            if (resultContext.Result is OkObjectResult okObjResult)
+            {
+                await cashService.SetCasheValueAsync(key, okObjResult.Value, TimeSpan.FromSeconds(durationInSeconds));
+
+            }
+        }
+
+        private string GenerateKey(HttpRequest request)
+        {
+            var key = new StringBuilder();
+            key.Append(request.Path);
+            foreach (var item in request.Query.OrderBy(x=>x.Key))
+            {
+                key.Append($"{item.Key}-{item.Value}");
+            }
+            return key.ToString();
+        }
+    }
+}
